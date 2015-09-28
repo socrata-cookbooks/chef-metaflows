9f4d7bc398bc07ccc6ef526ce9c8aae4c5d0f6b9
ele mooney <ele.mooney@socrata.com>
2015-09-28 14:06:48 -0700

    fixed typo in runit template

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ele.mooney@socrata.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-metaflows'
 long_description 'Installs/Configures chef-metaflows'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'runit'
