d4ceed9c371ac8aa42ab2a688f61aaed0f31d2b5
ele mooney <ele.mooney@socrata.com>
2015-09-30 15:47:49 -0700

    added sensor install

---
+++ recipes/sensor.rb
@@ -0,0 +1,14 @@
+# install and run the metaflows sensor/collector
+
+if node['platform'] == 'centos'
+  bash 'install metaflows sensor' do 
+    code <<-EOC
+      curl -O http://http://nsm.metaflows.com/updates/nsm_remote.zip
+      unzip nsm_remote.zip
+      cd nsm_remote
+      ./install_collector.sh
+      /nsm/etc/mss_collector.sh start
+      EOC
+    not_if { ::File.exists?('/nsm/etc/mss_collector.sh')
+  end
+end
