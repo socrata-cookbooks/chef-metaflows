03e0a903e1692880007f132c5df8269ca363c697
Matt Shanker <matthew.shanker@socrata.com>
2017-06-28 12:17:29 -0700

    ignore osquery traffic

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmin@socrata.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-metaflows'
 long_description 'Installs/Configures chef-metaflows'
-version          '0.5.0'
+version          '0.5.1'
 
 supports 'ubuntu'
 supports 'centos'
